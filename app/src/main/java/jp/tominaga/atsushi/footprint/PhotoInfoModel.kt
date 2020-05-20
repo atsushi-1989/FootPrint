d50fc07dc9740c9aca0efef692bf0b8eaff79a9d
atsushi-1989 <atushitomiaga5@gmail.com>
2020-05-20 14:57:21 +0900

    レイアウト画面：作成終了

---
+++ app/src/main/java/jp/tominaga/atsushi/footprint/PhotoInfoModel.kt
@@ -0,0 +1,19 @@
+package jp.tominaga.atsushi.footprint
+
+import io.realm.RealmObject
+
+open class PhotoInfoModel :RealmObject() {
+
+    //撮影した写真の画像ファイルのContentURI(アプリ間でファイル共有するための保存場所の表示形式)
+    var stringContentsUrl: String = ""
+    //撮影日時
+    var dataTime: String = ""
+    //経度
+    var latitude: Double = 0.0
+    //緯度
+    var longitude: Double = 0.0
+    //地点(緯度の文字列 + 経度の文字列) => 地図にマーキングする場合を特定させるため
+    var location: String =""
+    //コメント
+    var commen: String = ""
+}
