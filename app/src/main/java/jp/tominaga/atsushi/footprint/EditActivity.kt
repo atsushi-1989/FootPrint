e47bbdee13484e49fc39909baac4fc1921c2e992
atsushi-1989 <atushitomiaga5@gmail.com>
2020-05-25 12:00:17 +0900

    セクション8：終了

--- app/src/main/java/jp/tominaga/atsushi/footprint/EditActivity.kt
+++ app/src/main/java/jp/tominaga/atsushi/footprint/EditActivity.kt
@@ -1,12 +1,15 @@
 package jp.tominaga.atsushi.footprint
 
 import android.Manifest
+import android.app.Activity
 import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
 import android.widget.Toast
@@ -16,8 +19,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import com.davemorrissey.labs.subscaleview.ImageSource
 
 import kotlinx.android.synthetic.main.activity_edit.*
+import kotlinx.android.synthetic.main.content_edit.*
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -37,6 +42,8 @@ class EditActivity : AppCompatActivity() {
     var isLocationAccessEnabled = false
 
     var contentUri: Uri? = null
+
+    var selectedPhotoInfo = PhotoInfoModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,7 +65,12 @@ class EditActivity : AppCompatActivity() {
             //Todo 編集モードでやること
         }
 
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putParcelable(IntentKey.CONTENT_URI.name,contentUri)
     }
 
 
@@ -194,7 +206,51 @@ class EditActivity : AppCompatActivity() {
         val contentFileName = SimpleDateFormat("yyyyMMdd_HHmmss_z").format((Date()))
         contentUri = generateContentUriFromFileName(contentFileName)
 
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            putExtra(MediaStore.EXTRA_OUTPUT, contentUri)
+        }
 
+        //APIレベル未満の場合に必要な措置
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+            val context = applicationContext
+            val resolvedIntentActivites = context.packageManager
+                .queryIntentActivities(intent,PackageManager.MATCH_DEFAULT_ONLY)
+            for(resolvedIntentInfo in resolvedIntentActivites){
+                val packageName = resolvedIntentInfo.activityInfo.packageName
+                context.grantUriPermission(packageName,contentUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            }
+        }
+
+
+        startActivityForResult(intent, RQ_CODE_CAMERA)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode != Activity.RESULT_OK){
+            Toast.makeText(this@EditActivity,getString(R.string.shoot_failed),Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(requestCode != RQ_CODE_CAMERA){
+            Toast.makeText(this@EditActivity,getString(R.string.shoot_failed),Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(contentUri == null){
+            Toast.makeText(this@EditActivity,getString(R.string.shoot_failed),Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        imageView.setImage(ImageSource.uri(contentUri!!))
+        selectedPhotoInfo.stringContentsUrl = contentUri.toString()
+
+        selectedPhotoInfo.dataTime = SimpleDateFormat("yyyMMdd_HHmmss_z").format(Date())
+
+        //APIレベル21以下の場合に必要になる措置
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+            applicationContext.revokeUriPermission(contentUri,Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        }
     }
 
     private fun generateContentUriFromFileName(contentFileName: String): Uri? {
